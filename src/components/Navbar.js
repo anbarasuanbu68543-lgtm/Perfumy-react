f24c8b6ff7cc0cefc2b2cb862f234b788800bbea
anbarasuanbu68543-lgtm <anbarasuanbu68543@gmail.com>
2025-11-05 17:20:15 +0530

    updated Perfumy React app UI

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <di>
+      <nav>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>Home |</li>
+          <li>Product |</li>
+          <li>About |</li>
+          <li>Contact US |</li>
+        </ul>
+      </nav>
+    </di>
+  );
+}
+export default Navbar
